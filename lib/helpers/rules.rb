abe18ee17e85c497fc47caa4c62eba2eebc89f88
thomashaw <thomashaw@gmail.com>
2020-02-13 12:42:35 +0000

    WIP - push to pc (fix the escape string)

--- lib/helpers/rules.rb
+++ lib/helpers/rules.rb
@@ -11,14 +11,17 @@ class Rules
         case key
         when 'read_file'
           read_files = mod.goals[key]
-          read_files.each do |path|
-            rules << specific_elastalert_rule(path, 'r')
+          read_files.each do |entry|
+            paths = (entry.is_a? String) ? [entry] : entry
+            paths.each do |path|
+              rules << specific_elastalert_rule(path, 'r')
+            end
           end
         when 'modify_file'
         when 'access_account'
         when 'service_down'
         when 'system_down'
-        # TODO: Add exfiltration rule
+          # TODO: Add exfiltration rule
         else
         end
       end
@@ -59,26 +62,41 @@ class Rules
     "-w #{base_path} -p #{r_w} -k #{key}"
   end
 
+
+  # TODO: path == a string of an array - "["/etc/shadow", "/home/carolmiller/flag.txt"]"
+  # TODO: fix the eval problem + read arrays out into array format
   def self.specific_elastalert_rule(path, r_w)
+
     # TODO:  convert path into escaped path
     # TODO: add AND read/write events into rule
     # TODO: add AND some unique idenitifier for a user/vm (ip address or hostname are probably best)
     "name: #{get_ea_rulename(path)}\n" +
-    "type: any\n" +
-    "index: auditbeat-*\n" +
-    "filter:\n" +
-    "  - query:\n" +
-    "    query_string:\n" +
-    '      query: "combined_path: \\"*\\\/home\\\/vagrant\\\/*testfile\\"\""' + "\n" +
-    "alert:\n" +
-    "  - command\n" +
-    "command: [\"/usr/bin/tee\", \"-a\", \"/root/alerts\"]\n" +
-    "pipe_match_json: true\n"
+        "type: any\n" +
+        "index: auditbeat-*\n" +
+        "filter:\n" +
+        "  - query:\n" +
+        "    query_string:\n" +
+        '      query: "combined_path: ' + get_escaped_path(path) + "\n" +
+        "alert:\n" +
+        "  - command\n" +
+        "command: [\"/usr/bin/tee\", \"-a\", \"/root/alerts\"]\n" +
+        "pipe_match_json: true\n"
   end
 
   def self.get_ea_rulename(path)
     # TODO: Make this unique by combining the path with unique identifier for the scenario (e.g. IP address or hostname)
     return "example-rulename-#{rand(0..99999)}"
+  end
+
+  def self.get_escaped_path(path)
+    # TODO: implement
+    #'\\"*\\\/home\\\/vagrant\\\/*testfile\\"\""'
+    #'*/home/vagrant/*testfile'
+    #
+    # WIP:
+    # '*/home/vagrant/*testfile'.gsub('/','\\\\\/').gsub('/','\\\\/')    => "*\\\/home\\\/vagrant\\\/*testfile"
+
+    path
   end
 
 end
