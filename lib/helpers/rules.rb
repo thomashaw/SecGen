a7046c561cd34277ea76381775c5164835e3716b
ts <thomashaw@localhost.localdomain>
2020-02-14 13:06:18 +0000

    added get_hostname getter that sets once before returning

--- lib/helpers/rules.rb
+++ lib/helpers/rules.rb
@@ -8,7 +8,7 @@ class Rules
   # @type current valid values are ['elastalert', 'auditbeat']
   def self.generate_rules(system, type, mod)
     rules = []
-    hostname = system.hostname
+    hostname = system.get_hostname
     if type == 'elastalert'
       mod.goals.keys.each do |key|
         case key
