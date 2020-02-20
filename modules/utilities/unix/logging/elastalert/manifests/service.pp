56415b8aa9bf93033156d8ef75a6a1891589712d
ts <thomashaw@localhost.localdomain>
2020-02-20 11:49:18 +0000

    Elastalert: automatically create index (push to test)

--- modules/utilities/unix/logging/elastalert/manifests/service.pp
+++ modules/utilities/unix/logging/elastalert/manifests/service.pp
@@ -17,4 +17,23 @@ class elastalert::service {
     require => File[$ea_service_file],
   }
 
+  # Service to automatically create elastalert index, runs after reboot
+  file { '/etc/systemd/system/elastalert-index.service':
+    ensure => present,
+    source => 'puppet:///modules/elastalert/elastalert-index.service'
+  }
+
+  file { '/usr/local/bin/elastalert-index.rb':
+    ensure => file,
+    source => 'puppet:///modules/elastalert/elastalert-index.rb',
+    require => File[]
+  }
+
+  service { 'elastalert-index':
+    ensure => undef,
+    enable => true,
+    require => [File['/usr/local/bin/elastalert-index.rb'], File['/etc/systemd/system/elastalert-index.service']],
+    provider => 'systemd',
+    path => '/etc/systemd/system/'
+  }
 }
