5f640b1f273a5fa477bc28a1a81446e4cf3cf61a
thomashaw <thomashaw@gmail.com>
2020-02-21 15:20:47 +0000

    EA: downgrade, push to remote for testing

--- modules/utilities/unix/logging/elastalert/manifests/install.pp
+++ modules/utilities/unix/logging/elastalert/manifests/install.pp
@@ -2,20 +2,40 @@ class elastalert::install {
   $secgen_parameters = secgen_functions::get_parameters($::base64_inputs_file)
   $elasticsearch_ip = $secgen_parameters['elasticsearch_ip'][0]
   $elasticsearch_port = 0 + $secgen_parameters['elasticsearch_port'][0]
-  $diff_path = '/opt/elastalert/elastalert.diff'
+  $installdir = '/opt/elastalert/'
+  $source = 'http://github.com/Yelp/elastalert'
+  # $diff_path = '/opt/elastalert/elastalert.diff'
   Exec { path => ['/bin', '/usr/bin', '/usr/local/bin', '/sbin', '/usr/sbin'] }
 
-  ensure_packages('python3-pip')
-  ensure_packages(['elastalert', 'setuptools>=11.3', 'PyYAML>=5.1', 'elasticsearch==6.3.1'], { provider => 'pip3', require => [Package['python3-pip']] })
 
-  file { $diff_path:
-    ensure => file,
-    source => 'puppet:///modules/elastalert/elastalert.diff',
+  # Create directory to install into
+  file { $installdir:
+    ensure => directory,
   }
 
-  exec { 'apply diff patch':
-    command => "patch -p1 /usr/local/lib/python3.5/dist-packages/elastalert/alerts.py < $diff_path",
-    require => File[$diff_path],
+  # Clone elastalert from Github
+  vcsrepo { 'elastalert':
+    ensure   => present,
+    provider => git,
+    path     => $installdir,
+    source   => $source,
+    require  => File[$installdir],
+    revision => '98c7867',   # reset to 0.1.39
   }
+
+  ensure_packages(['python-pip','build-essential','libssl-dev','libffi-dev','python-dev'])
+  ensure_packages(['thehive4py','configparser>=3.5.0','setuptools>=11.3'], { provider => 'pip', require => [Package['python-pip']] })
+
+  # ensure_packages(['elastalert', 'setuptools>=11.3', 'PyYAML>=5.1', 'elasticsearch==6.3.1'], { provider => 'pip3', require => [Package['python3-pip']] })
+
+  # file { $diff_path:
+  #   ensure => file,
+  #   source => 'puppet:///modules/elastalert/elastalert.diff',
+  # }
+  #
+  # exec { 'apply diff patch':
+  #   command => "patch -p1 /usr/local/lib/python3.5/dist-packages/elastalert/alerts.py < $diff_path",
+  #   require => File[$diff_path],
+  # }
 
 }
