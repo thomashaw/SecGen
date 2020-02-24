d955f6f1016bc848258fcaf0707c4c9b917b1675
thomashaw <thomashaw@gmail.com>
2020-02-24 12:38:01 +0000

    EA: downgrade ES/elastalert (wip)

--- modules/utilities/unix/logging/elastalert/manifests/install.pp
+++ modules/utilities/unix/logging/elastalert/manifests/install.pp
@@ -4,19 +4,10 @@ class elastalert::install {
   $elasticsearch_port = 0 + $secgen_parameters['elasticsearch_port'][0]
   $installdir = '/opt/elastalert/'
   $source = 'http://github.com/Yelp/elastalert'
-  # $diff_path = '/opt/elastalert/elastalert.diff'
   Exec { path => ['/bin', '/usr/bin', '/usr/local/bin', '/sbin', '/usr/sbin'] }
 
   ensure_packages(['python-pip','build-essential','libssl-dev','libffi-dev','python-dev'])
-  ensure_packages(['thehive4py','configparser>=3.5.0','setuptools>=11.3'], { provider => 'pip', require => [Package['python-pip']] })
-  # ensure_packages(['elastalert', 'setuptools>=11.3', 'PyYAML>=5.1', 'elasticsearch==6.3.1'], { provider => 'pip3', require => [Package['python3-pip']] })
-
-  # apt upgrade cryptography
-  # exec { 'apt remove cryptography':
-  #   command => '/usr/bin/apt-get remove cryptography',
-  # }
-
-  ensure_packages(['cryptography>=2.8','mock>=2.0.0,<4.0.0', 'elasticsearch==6.3.1'], {provider => 'pip'})
+  ensure_packages(['thehive4py','configparser>=3.5.0','setuptools>=11.3', 'cryptography>=2.8','mock>=2.0.0,<4.0.0', 'elasticsearch==6.3.1'], { provider => 'pip', require => [Package['python-pip']] })
 
   # Create directory to install into
   file { $installdir:
@@ -33,14 +24,10 @@ class elastalert::install {
     revision => '98c7867',   # reset to 0.1.39
   }
 
-  # file { $diff_path:
-  #   ensure => file,
-  #   source => 'puppet:///modules/elastalert/elastalert.diff',
-  # }
-  #
-  # exec { 'apply diff patch':
-  #   command => "patch -p1 /usr/local/lib/python3.5/dist-packages/elastalert/alerts.py < $diff_path",
-  #   require => File[$diff_path],
-  # }
+  exec { 'setup.py install':
+    command => '/usr/bin/python2.7 setup.py install',
+    cwd => '/opt/elastalert',
+    require => Vcsrepo['Elastalert'],
+  }
 
 }
