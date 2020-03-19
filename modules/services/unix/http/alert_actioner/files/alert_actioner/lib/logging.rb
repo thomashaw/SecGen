16560e3a803682b8069502c5057e130c6d9c2cfc
ts <thomashaw@localhost.localdomain>
2020-03-19 13:13:01 +0000

    wip on running notify-send message commands

--- modules/services/unix/http/alert_actioner/files/alert_actioner/lib/logging.rb
+++ modules/services/unix/http/alert_actioner/files/alert_actioner/lib/logging.rb
@@ -1,10 +1,15 @@
 module Logging
+  require 'logger'
   require_relative 'aa_constants'
   def logger
     Logging.logger
   end
 
   def self.logger
-    @logger ||= Logger.new(ALERTER_DIRECTORY + 'alert-router.log')
+    # TODO: remove me
+    alerter_directory = '/home/thomashaw/git/SecGen/'
+    @logger ||= Logger.new(alerter_directory + 'alert-router.log')
+    # TODO: uncomment me
+    # @logger ||= Logger.new(ALERTER_DIRECTORY + 'alert-router.log')
   end
 end
