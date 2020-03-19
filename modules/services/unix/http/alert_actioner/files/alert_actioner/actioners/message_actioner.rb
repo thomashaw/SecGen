d793a795f416c24dea91c9d26ea5eb8b1a3a4f5f
ts <thomashaw@localhost.localdomain>
2020-03-19 16:42:11 +0000

    wip: MessageActioner and CommandActioner changes - now uses bash script and can handle scripts/multiple commands. push to remote for testing.

--- modules/services/unix/http/alert_actioner/files/alert_actioner/actioners/message_actioner.rb
+++ modules/services/unix/http/alert_actioner/files/alert_actioner/actioners/message_actioner.rb
@@ -2,26 +2,26 @@ require_relative 'command_actioner'
 
 class MessageActioner < CommandActioner
 
-  def initialize(config_filename, alertaction_index, alert_name, host, username, password, message)
-    super(config_filename, alertaction_index, alert_name, host, username, password, "bash -s < ./#{msg_script}" message)
+  attr_accessor :message_header
+  attr_accessor :message_subtext
+
+  def initialize(config_filename, alertaction_index, alert_name, host, username, password, message_header, message_subtext)
+    super(config_filename, alertaction_index, alert_name, host, username, password)
+    self.message_header = message_header
+    self.message_subtext = message_subtext
   end
+
+  # Return [Array] of command strings
+  def command_strings
+    ["DISPLAY=:0 /usr/bin/notify-send -u critical '#{self.message_header}' '#{self.message_subtext}'"]
+  end
+
 
   # TODO: Override me in superclass to print actioner type + all parameters??
   def to_s
-    "MessageActioner:\n TODO: to_s parameters"
+    "MessageActioner:\n  Message Header: #{self.message_header}\n  Message Subtext: #{self.message_subtext}"
   end
 
 end
 
-MessageActioner.new('config_filename', 1, 'example-alert', '127.0.0.1', 'tg5196571lap', ['-u critical', "'Well done'", "'here is some subtext'" '"']).action_alert
-
-
-# TODO: Convert the below into the automatic creation of a_script.sh (based on the message etc) and then the running of it via sshpass/ssh
-# TODO: Update the parameters to include the username of the account we want to inform, and their password
-# TODO: Play around with this stuff on the VMs. There could be an easier way to graphically alert all users on KDE/XFCE and we can forget about gnome.
-#
-# [root@localhost thomashaw]# sshpass -p tg5196571lap ssh -T -oStrictHostKeyChecking=no thomashaw@127.0.0.1 bash -s < ./a_script.sh
-# touch: cannot touch '/home/thomashaw/new_test': Permission denied
-# [root@localhost thomashaw]# cat a_script.sh
-# #!/bin/bash
-# DISPLAY=:0 /usr/bin/notify-send -u critical 'Well done' 'here is some subtext'
+MessageActioner.new('config_filename', 1, 'example-alert', '127.0.0.1', 'thomashaw', 'tg5196571lap',  'Well done', 'here is some subtext').action_alert
--- modules/services/unix/http/alert_actioner/files/alert_actioner/lib/aa_constants.rb
+++ modules/services/unix/http/alert_actioner/files/alert_actioner/lib/aa_constants.rb
@@ -1,4 +1,7 @@
-ALERTER_DIRECTORY = '/opt/alert_actioner/'
+# ALERTER_DIRECTORY = '/opt/alert_actioner/'
+ALERTER_DIRECTORY = '/home/thomashaw/git/SecGen/modules/services/unix/http/alert_actioner/files/alert_actioner/'
 CONFIG_DIRECTORY = ALERTER_DIRECTORY + 'config/'
 LIB_DIRECTORY = ALERTER_DIRECTORY + 'lib/'
 AA_CONFIG_SCHEMA = LIB_DIRECTORY + 'alertactioner_config_schema.xsd'
+TEMPLATES_DIRECTORY = ALERTER_DIRECTORY + 'templates/'
+COMMANDS_DIRECTORY = CONFIG_DIRECTORY + 'commands/'
