de16645220d0aace94bcbb226bccbbb6038167da
thomashaw <thomashaw@gmail.com>
2020-03-20 11:30:43 +0000

    alertactioner_config_schema: fixed paths for testing on remote, added sshpass

--- modules/services/unix/http/alert_actioner/files/alert_actioner/lib/logging.rb
+++ modules/services/unix/http/alert_actioner/files/alert_actioner/lib/logging.rb
@@ -6,10 +6,9 @@ module Logging
   end
 
   def self.logger
-    # TODO: remove me
-    alerter_directory = '/home/thomashaw/git/SecGen/'
-    @logger ||= Logger.new(alerter_directory + 'alert-router.log')
-    # TODO: uncomment me
-    # @logger ||= Logger.new(ALERTER_DIRECTORY + 'alert-router.log')
+    # TODO: for local testing uncomment me
+    # alerter_directory = '/home/thomashaw/git/SecGen/'
+    # @logger ||= Logger.new(alerter_directory + 'alert-router.log')
+    @logger ||= Logger.new(ALERTER_DIRECTORY + 'alert-router.log')
   end
 end
