6696328b681e99e306b19bfe9ed5d55ba6ba16aa
ts <thomashaw@localhost.localdomain>
2020-03-30 20:00:09 +0100

    (wip) minor changes... test on remote

--- modules/services/unix/logging/analysis_alert_action_server/analysis_alert_action_server.pp
+++ modules/services/unix/logging/analysis_alert_action_server/analysis_alert_action_server.pp
@@ -13,9 +13,9 @@ $secgen_parameters = secgen_functions::get_parameters($::base64_inputs_file)
 $aaa_config = $secgen_parameters['aaa_config'][0]
 $elasticsearch_ip = $aaa_config['server_ip'][0]
 $elasticsearch_port = 0 + $aaa_config['elasticsearch_port'][0]
-$logstash_port = 0 + $secgen_parameters['logstash_port'][0]
+$logstash_port = 0 + $aaa_config['logstash_port'][0]
 $kibana_ip = $aaa_config['server_ip'][0]
-$kibana_port = 0 + $secgen_parameters['kibana_port'][0]
+$kibana_port = 0 + $aaa_config['kibana_port'][0]
 # $agent_name = $secgen_parameters['wazuh_agent_name'][0]
 
 
