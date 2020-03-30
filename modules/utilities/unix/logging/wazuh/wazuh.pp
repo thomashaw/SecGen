85ecd00954dcd0b253a2c648a7300cfc0d2ff122
ts <thomashaw@localhost.localdomain>
2020-03-30 19:57:04 +0100

    (wip) Only run the default individual module behaviour if the aaa modules are not defined.

--- modules/utilities/unix/logging/wazuh/wazuh.pp
+++ modules/utilities/unix/logging/wazuh/wazuh.pp
@@ -1,27 +1,29 @@
-$secgen_parameters = secgen_functions::get_parameters($::base64_inputs_file)
-$component = $secgen_parameters['component'][0]
-$kibana_elasticsearch_ip = $secgen_parameters['kibana_ip'][0]
-$agent_name = $secgen_parameters['wazuh_agent_name'][0]
+unless defined('analysis_alert_action_server') or defined('analysis_alert_action_client') {
+  $secgen_parameters = secgen_functions::get_parameters($::base64_inputs_file)
+  $component = $secgen_parameters['component'][0]
+  $kibana_elasticsearch_ip = $secgen_parameters['kibana_ip'][0]
+  $agent_name = $secgen_parameters['wazuh_agent_name'][0]
 
-if ($component == 'server') {
-  class { '::wazuh::manager':
-    ossec_smtp_server   => 'localhost',
-    ossec_emailto => ['user@mycompany.com'],
-    agent_auth_password => '6663484170b2c69451e01ba11f319533', #todo: obviously fix this - must be 32char
-  }
-  class { '::wazuh::kibana':
-    kibana_elasticsearch_ip => $kibana_elasticsearch_ip,
-  }
+  if ($component == 'server') {
+    class { '::wazuh::manager':
+      ossec_smtp_server   => 'localhost',
+      ossec_emailto       => ['user@mycompany.com'],
+      agent_auth_password => '6663484170b2c69451e01ba11f319533', #todo: obviously fix this - must be 32char
+    }
+    class { '::wazuh::kibana':
+      kibana_elasticsearch_ip => $kibana_elasticsearch_ip,
+    }
 
-  exec{ 'enable ossec auth':
-    command => '/var/ossec/bin/ossec-control enable auth',
-    require => Class['::wazuh::manager'],
-  }
+    exec { 'enable ossec auth':
+      command => '/var/ossec/bin/ossec-control enable auth',
+      require => Class['::wazuh::manager'],
+    }
 
-} elsif ($component == 'client') {
-  class { "::wazuh::agent":
-    wazuh_register_endpoint => $kibana_elasticsearch_ip,
-    wazuh_reporting_endpoint => $kibana_elasticsearch_ip,
-    agent_name => $agent_name,
+  } elsif ($component == 'client') {
+    class { "::wazuh::agent":
+      wazuh_register_endpoint  => $kibana_elasticsearch_ip,
+      wazuh_reporting_endpoint => $kibana_elasticsearch_ip,
+      agent_name               => $agent_name,
+    }
   }
 }
