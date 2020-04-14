6c2bbca991987ab4dc9ad25cfc89bb6a6c8c473e
thomashaw <thomashaw@gmail.com>
2020-04-14 19:28:36 +0100

    1DEV-51: aaa_server: remove alert actioner module and merge it into aaa_server

--- modules/services/unix/logging/analysis_alert_action_server/files/alert_actioner/lib/aa_constants.rb
+++ modules/services/unix/logging/analysis_alert_action_server/files/alert_actioner/lib/aa_constants.rb
@@ -1,6 +1,6 @@
 ALERTER_DIRECTORY = '/opt/alert_actioner/'
 # TODO: for testing
-# ALERTER_DIRECTORY = '/home/thomashaw/git/SecGen/modules/services/unix/http/alert_actioner/files/alert_actioner/'
+# ALERTER_DIRECTORY = '/home/thomashaw/git/SecGen/modules/services/unix/http/analysis_alert_action_server/files/alert_actioner/'
 CONFIG_DIRECTORY = ALERTER_DIRECTORY + 'config/'
 LIB_DIRECTORY = ALERTER_DIRECTORY + 'lib/'
 AA_CONFIG_SCHEMA = LIB_DIRECTORY + 'alertactioner_config_schema.xsd'
