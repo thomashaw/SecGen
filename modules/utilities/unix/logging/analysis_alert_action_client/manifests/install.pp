a0916c4eebba09f62a5b843f44e19401fa729974
thomashaw <thomashaw@gmail.com>
2020-04-16 15:48:19 +0100

    different mail notification approach for kali

--- modules/utilities/unix/logging/analysis_alert_action_client/manifests/install.pp
+++ modules/utilities/unix/logging/analysis_alert_action_client/manifests/install.pp
@@ -1,5 +1,17 @@
 class analysis_alert_action_client::install {
-  package{ ['mailutils', 'libnotify-bin', 'mail-notification']:
+  package{ ['mailutils', 'libnotify-bin']:
     ensure => installed,
   }
+
+  case $::lsbdistcodename {
+    'stretch': {
+      package { 'mail-notification': ensure => installed }
+      # TODO: Add config stuff for mail-notification on debian desktop
+    }
+    'kali-rolling': {
+      package { 'xfce-mailwatch-plugin': ensure => installed }
+      # TODO: Add config stuff for mailwatch on kali desktop
+    }
+  }
+
 }
