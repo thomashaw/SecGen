29311dcce05d7a361fe8219f01f487be5120a37f
thomashaw <thomashaw@gmail.com>
2020-04-21 13:25:02 +0100

    Removing mail notification installers

--- modules/utilities/unix/logging/analysis_alert_action_client/manifests/install.pp
+++ modules/utilities/unix/logging/analysis_alert_action_client/manifests/install.pp
@@ -3,15 +3,15 @@ class analysis_alert_action_client::install {
     ensure => installed,
   }
 
-  case $::lsbdistcodename {
-    'stretch': {
-      package { 'mail-notification': ensure => installed }
+  # TODO: Update this - none of this works! Try the $MAILCHECK env variable with /bin/bash...
+  # case $::lsbdistcodename {
+    # 'stretch': {
+    #   package { 'mail-notification': ensure => installed }
       # TODO: Add config stuff for mail-notification on debian desktop
-    }
-    'kali-rolling': {
-      package { 'xfce4-mailwatch-plugin': ensure => installed }
+    # }
+    # 'kali-rolling': {
+    #   package { 'xfce4-mailwatch-plugin': ensure => installed }
       # TODO: Add config stuff for mailwatch on kali desktop
-    }
-  }
-
+    # }
+  # }
 }
