978f2623ef7fdbee825c58319cc3be69c209a912
thomashaw <thomashaw@gmail.com>
2021-11-22 21:12:59 +0000

    Module Updates

--- modules/vulnerabilities/unix/access_control_misconfigurations/readable_shadow/manifests/config.pp
+++ modules/vulnerabilities/unix/access_control_misconfigurations/readable_shadow/manifests/config.pp
@@ -1,6 +1,15 @@
 class readable_shadow::config {
+  $secgen_parameters = secgen_functions::get_parameters($::base64_inputs_file)
+  $shadow_flag = $strings_to_leak[0]
+
   file { '/etc/shadow':
     ensure  => present,
     mode    => '0644',
+  }
+
+  if $shadow_flag {
+    exec{ 'append_flag_to_etc_shadow':
+      command => "/bin/echo '$shadow_flag' >> /etc/shadow"
+    }
   }
 }
