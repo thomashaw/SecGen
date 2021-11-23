483557082c8e1cd7e7f8be11e09c467b64d42a8b
thomashaw <thomashaw@gmail.com>
2021-11-23 13:48:13 +0000

    readable_shadow: empty strings_to_leak fix

--- modules/vulnerabilities/unix/access_control_misconfigurations/readable_shadow/manifests/config.pp
+++ modules/vulnerabilities/unix/access_control_misconfigurations/readable_shadow/manifests/config.pp
@@ -1,14 +1,14 @@
 class readable_shadow::config {
   $secgen_parameters = secgen_functions::get_parameters($::base64_inputs_file)
   $strings_to_leak = $secgen_parameters['strings_to_leak']
-  $shadow_flag = $strings_to_leak[0]
 
   file { '/etc/shadow':
     ensure  => present,
     mode    => '0644',
   }
 
-  if $shadow_flag {
+  if $strings_to_leak and $strings_to_leak[0] {
+    $shadow_flag = $strings_to_leak[0]
     exec{ 'append_flag_to_etc_shadow':
       command => "/bin/echo '$shadow_flag' >> /etc/shadow"
     }
