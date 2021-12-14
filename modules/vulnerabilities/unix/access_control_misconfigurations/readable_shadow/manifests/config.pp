37a7045dbbd93cf5eeb9b52e5a0a4737f8241a9f
thomashaw <thomashaw@gmail.com>
2021-12-14 21:01:50 +0000

    readable_shadow: fix for no read_fact values

--- modules/vulnerabilities/unix/access_control_misconfigurations/readable_shadow/manifests/config.pp
+++ modules/vulnerabilities/unix/access_control_misconfigurations/readable_shadow/manifests/config.pp
@@ -1,16 +1,18 @@
 class readable_shadow::config {
-  $secgen_parameters = secgen_functions::get_parameters($::base64_inputs_file)
-  $strings_to_leak = $secgen_parameters['strings_to_leak']
-
   file { '/etc/shadow':
-    ensure  => present,
-    mode    => '0644',
+    ensure => present,
+    mode   => '0644',
   }
 
-  if $strings_to_leak and $strings_to_leak[0] {
-    $shadow_flag = $strings_to_leak[0]
-    exec{ 'append_flag_to_etc_shadow':
-      command => "/bin/echo '$shadow_flag' >> /etc/shadow"
+  # Check if base64 inputs file is included as it's only generated when a string has been leaked from the scenario
+  if $::base64_inputs_file {
+    $secgen_parameters = secgen_functions::get_parameters($::base64_inputs_file)
+    $strings_to_leak = $secgen_parameters['strings_to_leak']
+    if $strings_to_leak and $strings_to_leak[0] {
+      $shadow_flag = $strings_to_leak[0]
+      exec { 'append_flag_to_etc_shadow':
+        command => "/bin/echo '$shadow_flag' >> /etc/shadow"
+      }
     }
   }
 }
