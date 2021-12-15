278fcb4a9aab2a186389b4f61d8e439f227d192c
thomashaw <thomashaw@gmail.com>
2021-12-15 01:30:29 +0000

    WIP - Testing local web_actioner changes

--- modules/services/unix/logging/analysis_alert_action_server/files/alert_actioner/lib/aa_constants.rb
+++ modules/services/unix/logging/analysis_alert_action_server/files/alert_actioner/lib/aa_constants.rb
@@ -1,4 +1,5 @@
-ALERTER_DIRECTORY = File.expand_path(File.dirname(__FILE__))
+require 'pathname'
+ALERTER_DIRECTORY = (Pathname.new(File.expand_path(File.dirname(__FILE__))).parent.to_s) + "/"
 CONFIG_DIRECTORY = ALERTER_DIRECTORY + 'config/'
 LIB_DIRECTORY = ALERTER_DIRECTORY + 'lib/'
 AA_CONFIG_SCHEMA = LIB_DIRECTORY + 'alertactioner_config_schema.xsd'
