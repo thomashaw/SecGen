a44a8bfec2751515086d7a4199da0d6afd8530bc
thomashaw <thomashaw@gmail.com>
2022-02-08 15:15:30 +0000

    wip

--- modules/build/puppet/secgen_functions/manifests/leak_file.pp
+++ modules/build/puppet/secgen_functions/manifests/leak_file.pp
@@ -1,7 +1,17 @@
-define secgen_functions::leak_file($file_path_to_leak, $leaked_filename, $storage_directory, $strings_to_leak, $owner = 'root', $group = 'root', $mode = '0660', $leaked_from = '' ) {
+define secgen_functions::leak_file (
+  $file_path_to_leak,
+  $leaked_filename,
+  $storage_directory,
+  $strings_to_leak,
+  $owner       = 'root',
+  $group       = 'root',
+  $mode        = '0660',
+  $leaked_from = ''
+) {
 
-  if (($file_path_to_leak and $file_path_to_leak != '') and (($leaked_filename and $leaked_filename != '') or ($storage_directory and $storage_directory != ''))) {
-    fail ("ERROR: Either a file_path_to_leak OR (a leaked_filename and a storage directory), not both.")
+  if (($file_path_to_leak and $file_path_to_leak != '') and
+    (($leaked_filename and $leaked_filename != '') or ($storage_directory and $storage_directory != ''))) {
+    fail("ERROR: Either a file_path_to_leak OR (a leaked_filename and a storage directory), not both.")
   }
 
   if ($leaked_filename != '') {
@@ -9,60 +19,59 @@ define secgen_functions::leak_file($file_path_to_leak, $leaked_filename, $storag
   } else {
     $path_to_leak = $file_path_to_leak
   }
-    # create the directory tree, incase the file name has extra layers of directories
-    if $::osfamily == 'windows' {
-      exec { "win_$leaked_from-$path_to_leak-mkdir":
-        command => "cmd.exe /c md $storage_directory",
-        path => "C:\\Windows\\System32",
-        returns => [1]
-      } ->
-      exec { "win_$leaked_from-$path_to_leak-takeown":
-        command => "cmd.exe /c takeown /F $storage_directory /U $owner",
-        path => "C:\\Windows\\System32",
-        returns => [1]
-      }
+  # create the directory tree, incase the file name has extra layers of directories
+  if $::osfamily == 'windows' {
+    exec { "win_$leaked_from-$path_to_leak-mkdir":
+      command => "cmd.exe /c md $storage_directory",
+      path    => "C:\\Windows\\System32",
+      returns => [1]
+    } ->
+    exec { "win_$leaked_from-$path_to_leak-takeown":
+      command => "cmd.exe /c takeown /F $storage_directory /U $owner",
+      path    => "C:\\Windows\\System32",
+      returns => [1]
+    }
     # assuming everything not windows has mkdir and chown
-    } else {
-      exec { "$leaked_from-$path_to_leak-mkdir":
-        path    => ['/bin', '/usr/bin', '/usr/local/bin', '/sbin', '/usr/sbin'],
-        command => "mkdir -p `dirname $path_to_leak`;chown $owner. `dirname $path_to_leak`",
-        provider => shell,
-      }
-
+  } else {
+    exec { "$leaked_from-$path_to_leak-mkdir":
+      path     => ['/bin', '/usr/bin', '/usr/local/bin', '/sbin', '/usr/sbin'],
+      command  => "mkdir -p `dirname $path_to_leak`;chown $owner. `dirname $path_to_leak`",
+      provider => shell,
     }
 
-    # If the file already exists append to it, otherwise create it.
-    if $::osfamily == 'windows' {
-        if (defined(File[$path_to_leak])){
-          notice("File with that name already defined, appending leaked strings instead...")
-          file_line { "$leaked_from-$path_to_leak -append":
-            path    => $path_to_leak ,
-            line => "\n------\n$strings_to_leak",
-          }
-        } else {
-            file { $path_to_leak:
-              ensure   => present,
-              owner    => $owner,
-              group    => $group,
-              content  => template('secgen_functions/overshare.erb')
-            }
-          }
-      }else{  # If the file already exists append to it, otherwise create it.
-        if (defined(File[$path_to_leak])){
-          notice("File with that name already defined, appending leaked strings instead...")
-          file_line { "$leaked_from-$path_to_leak -append":
-            path    => $path_to_leak ,
-            line => "\n------\n$strings_to_leak",
-          }
-        } else {
-            file { $path_to_leak:
-              ensure   => present,
-              owner    => $owner,
-              group    => $group,
-              mode     => $mode,
-              content  => template('secgen_functions/overshare.erb')
-            }
-          }
-        }
+  }
+
+  # If the file already exists append to it, otherwise create it.
+  if $::osfamily == 'windows' {
+    if (defined(File[$path_to_leak])) {
+      notice("File with that name already defined, appending leaked strings instead...")
+      file_line { "$leaked_from-$path_to_leak -append":
+        path => $path_to_leak,
+        line => "\n------\n$strings_to_leak",
+      }
+    } else {
+      file { $path_to_leak:
+        ensure  => present,
+        owner   => $owner,
+        group   => $group,
+        content => template('secgen_functions/overshare.erb')
+      }
+    }
+  }else { # If the file already exists append to it, otherwise create it.
+    if (defined(File[$path_to_leak])) {
+      notice("File with that name already defined, appending leaked strings instead...")
+      file_line { "$leaked_from-$path_to_leak -append":
+        path => $path_to_leak,
+        line => "\n------\n$strings_to_leak",
+      }
+    } else {
+      file { $path_to_leak:
+        ensure  => present,
+        owner   => $owner,
+        group   => $group,
+        mode    => $mode,
+        content => template('secgen_functions/overshare.erb')
+      }
+    }
   }
 }
