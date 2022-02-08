d936b5757ba9739ca4de9721b100f1760cb76cc9
thomashaw <thomashaw@gmail.com>
2022-02-08 18:38:13 +0000

    update pip3

--- modules/utilities/unix/logging/elastalert/manifests/install.pp
+++ modules/utilities/unix/logging/elastalert/manifests/install.pp
@@ -2,7 +2,13 @@ class elastalert::install ($elasticsearch_ip, $elasticsearch_port,$installdir = 
   Exec { path => ['/bin', '/usr/bin', '/usr/local/bin', '/sbin', '/usr/sbin'] }
 
   ensure_packages(['python3-pip','build-essential','libssl-dev','libffi-dev','python-dev', 'supervisor' ])
-  ensure_packages(['PyYAML>=5.1','urllib3>=1.26.7','elastalert'], { provider => 'pip3', require => [Package['python3-pip']] })
+
+  exec { 'run pip3 update':
+    command => '/usr/bin/python3 -m pip3 install --upgrade pip',
+    require => Package['python3-pip']
+  }
+
+  ensure_packages(['PyYAML>=5.1','urllib3>=1.26.7','elastalert'], { provider => 'pip3', require => [Package['python3-pip'], Exec['run pip3 update']] })
 
   # Create directory to install into   TODO: Change this to another variable name.  Should put configs in /etc/ probably if we're installing via...
   file { $installdir:
