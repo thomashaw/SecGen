236f6dc4ba13b61df79a9b7600422ad33ab189d3
thomashaw <thomashaw@gmail.com>
2022-03-23 20:09:53 +0000

    add db user cont.

--- modules/services/unix/logging/analysis_alert_action_server/files/postgresql_setup.sh
+++ modules/services/unix/logging/analysis_alert_action_server/files/postgresql_setup.sh
@@ -2,7 +2,7 @@
 
 USERNAME=${1}
 PASSWORD=${2}
-
+sudo -u postgres createuser ${USERNAME}
 sudo -u postgres psql -c "DROP DATABASE IF EXISTS alert_events;"
 sudo -u postgres psql -c "CREATE DATABASE alert_events;"
 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE alert_events TO ${USERNAME};"
