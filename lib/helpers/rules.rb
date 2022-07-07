ca1701a86e3fa7d83420dcfd7a77daedfdc4be44
thomashaw <thomashaw@gmail.com>
2022-07-07 22:38:42 +0100

    wip

--- lib/helpers/rules.rb
+++ lib/helpers/rules.rb
@@ -69,9 +69,10 @@ class Rules
         "filter:\n" +
         "  - query:\n" +
         "      query_string:\n" +
-        '        query: "combined_path: \"' + goal['file_path'] + '\" AND auditd.result: success AND event.action: opened-file AND (process.executable: \"/bin/cat\" OR process.executable: \"/usr/bin/vim.basic")"' + "\n" +
+        '        query: "combined_path: \"' + goal['file_path'] + '\" AND auditd.result: success AND event.action: opened-file AND (process.executable: \"/bin/cat\" OR process.executable: \"/usr/bin/vim.basic\" OR process.executable: \"/bin/less\" OR process.executable: \"/bin/more\")"' + "\n" +
 
-
+        # Different OR clause in EA
+        #
         # TODO: WIP - improve this rule!
         #
         #
