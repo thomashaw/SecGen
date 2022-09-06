2420f3ecae5c7af1ca510be540f136b7899674ed
thomashaw <thomashaw@gmail.com>
2022-09-06 20:13:11 +0100

    Goal message granular mappings (WiP)

--- modules/generators/structured_content/alert_actioner_config/goal_message_map/secgen_local/local.rb
+++ modules/generators/structured_content/alert_actioner_config/goal_message_map/secgen_local/local.rb
@@ -3,47 +3,49 @@ require 'json'
 require_relative '../../../../../../lib/objects/local_string_generator.rb'
 
 # Generate a config hash for the XmlAlertActionConfigGenerator
-class GoalMessageHost < StringGenerator
+class GoalMessageMap < StringGenerator
   attr_accessor :host
   attr_accessor :sender
   attr_accessor :password
   attr_accessor :recipient
   attr_accessor :message_header
   attr_accessor :message_subtext
-  attr_accessor :mappings
+  attr_accessor :mapping
   attr_accessor :mapping_type
 
   def initialize
     super
     self.module_name = 'Goal-Message-Host AlertActioner Config Generator'
+    self.unique_id = ''     # Unique ID for the module selector (or scenario level goal e.g. scenariogoal1)
     self.host = ''          # Host IP
     self.sender = ''      # Host username
     self.password = ''      # Host password
     self.recipient = ''      # Host password
     self.message_header = ''       # Message to send to host
     self.message_subtext = ''       # Message to send to host
-    self.mappings = []
+    self.mapping = []       # TODO: Implement granular mappings
     self.mapping_type = ''
   end
 
   def generate
-    self.outputs << {:host => self.host, :sender => self.sender, :password => self.password, :recipient => self.recipient, :message_header => self.message_header, :message_subtext => self.message_subtext, :mapping => self.mapping, :mapping_type => self.mapping_type}.to_json
+    self.outputs << { :unique_id => self.unique_id, :host => self.host, :sender => self.sender, :password => self.password, :recipient => self.recipient, :message_header => self.message_header, :message_subtext => self.message_subtext, :mapping => self.mapping, :mapping_type => self.mapping_type}.to_json
   end
 
   def get_options_array
-    super + [['--host', GetoptLong::REQUIRED_ARGUMENT],
-             ['--sender', GetoptLong::REQUIRED_ARGUMENT],
-             ['--password', GetoptLong::REQUIRED_ARGUMENT],
-             ['--recipient', GetoptLong::REQUIRED_ARGUMENT],
-             ['--message_header', GetoptLong::REQUIRED_ARGUMENT],
-             ['--message_subtext', GetoptLong::OPTIONAL_ARGUMENT],
-             ['--mappings', GetoptLong::OPTIONAL_ARGUMENT],
-             ['--mapping_type', GetoptLong::OPTIONAL_ARGUMENT]]
+    super + [['--unique_id', GetoptLong::REQUIRED_ARGUMENT],
+             ['--host', GetoptLong::OPTIONAL_ARGUMENT],
+             ['--sender', GetoptLong::OPTIONAL_ARGUMENT],
+             ['--password', GetoptLong::OPTIONAL_ARGUMENT],
+             ['--recipient', GetoptLong::OPTIONAL_ARGUMENT],
+             ['--message_header', GetoptLong::OPTIONAL_ARGUMENT],
+             ['--message_subtext', GetoptLong::OPTIONAL_ARGUMENT]]
   end
 
   def process_options(opt, arg)
     super
     case opt
+    when '--unique_id'
+      self.unique_id = arg
     when '--host'
       self.host = arg
     when '--sender'
@@ -56,12 +58,8 @@ class GoalMessageHost < StringGenerator
       self.message_header = arg
     when '--message_subtext'
       self.message_subtext = arg
-    when '--mappings'
-      self.mappings << arg
-    when '--mapping_type'
-      self.mapping_type = arg
     end
   end
 end
 
-GoalMessageHost.new.run
+GoalMessageMap.new.run
