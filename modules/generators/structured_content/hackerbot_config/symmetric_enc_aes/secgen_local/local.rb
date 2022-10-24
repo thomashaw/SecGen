78504afb938509b39c545f811c2361c0b4a5cc00
thomashaw <thomashaw@gmail.com>
2022-10-24 14:19:05 +0100

    CSL: AES Lab (wip 4/?)
    (cherry picked from commit 06996f4d1c1162beb09886dbf2c8e7b3eabd48a4)

--- modules/generators/structured_content/hackerbot_config/symmetric_enc_aes/secgen_local/local.rb
+++ modules/generators/structured_content/hackerbot_config/symmetric_enc_aes/secgen_local/local.rb
@@ -1,19 +1,19 @@
 #!/usr/bin/ruby
 require_relative '../../../../../../lib/objects/local_hackerbot_config_generator.rb'
 
-class Integrity1 < HackerbotConfigGenerator
+class SymmetricEncAES < HackerbotConfigGenerator
 
   def initialize
     super
-    self.module_name = 'Hackerbot Config Generator Integrity'
-    self.title = 'Integrity management: protecting integrity'
+    self.module_name = 'Hackerbot Config Generator Symmetric/AES'
+    self.title = 'Symmetric Encryption with Advanced Encryption Standard (AES)'
 
     self.local_dir = File.expand_path('../../',__FILE__)
     self.templates_path = "#{self.local_dir}/templates/"
-    self.config_template_path = "#{self.local_dir}/templates/integrity_lab.xml.erb"
+    self.config_template_path = "#{self.local_dir}/templates/symmetric_enc_aes_lab.xml.erb"
     self.html_template_path = "#{self.local_dir}/templates/labsheet.html.erb"
   end
 
 end
 
-Integrity1.new.run
+SymmetricEncAES.new.run
