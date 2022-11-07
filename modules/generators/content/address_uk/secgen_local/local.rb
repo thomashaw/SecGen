fca33945d1ae2e99a9a7e7ea633cf464fb0a184a
thomashaw <thomashaw@gmail.com>
2022-11-07 13:23:40 +0000

    Disable uk specific addresses for now, bug in faker library for config.locale = en-GB)

--- modules/generators/content/address_uk/secgen_local/local.rb
+++ modules/generators/content/address_uk/secgen_local/local.rb
@@ -9,14 +9,16 @@ class UKAddressGenerator < StringGenerator
   end
 
   def generate
-    Faker::Config.locale = 'en-GB'
+    # Faker::Config.locale = 'en-GB'
+    # TODO: Currently a bug in Faker library with en-gb, use default for now.
 
     street_name = Faker::Address.street_address
     city = Faker::Address.city
-    county = Faker::Address.county
+    # county = Faker::Address.county
     postcode = Faker::Address.postcode
 
-    self.outputs << street_name + ', ' + city + ', ' + county + ', ' + postcode
+    # self.outputs << street_name + ', ' + city + ', ' + county + ', ' + postcode
+    self.outputs << street_name + ', ' + city + ', ' + postcode
   end
 end
 
