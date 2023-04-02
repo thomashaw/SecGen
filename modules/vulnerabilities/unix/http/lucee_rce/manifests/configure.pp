c9d79cd9a1cdd1540fc272c5c4b37cfae31bcc0e
JD <j.davis2344@student.leedsbeckett.ac.uk>
2023-04-02 01:04:50 +0100

    final changes for review

--- modules/vulnerabilities/unix/http/lucee_rce/manifests/configure.pp
+++ modules/vulnerabilities/unix/http/lucee_rce/manifests/configure.pp
@@ -2,13 +2,14 @@
 # Configuration for lucee with secgen
 #
 class lucee_rce::configure {
-  $leaked_filenames = ['flagtest'] ##$secgen_parameters['leaked_filenames']
-  $strings_to_leak = ['this is a list of strings that are secrets / flags','another secret'] ##$secgen_parameters['strings_to_leak']
+  $secgen_parameters = secgen_functions::get_parameters($::base64_inputs_file)
+  $leaked_filenames = $secgen_parameters['leaked_filenames']
+  $strings_to_leak = $secgen_parameters['strings_to_leak']
 
   Exec { path => ['/bin', '/usr/bin', '/usr/local/bin', '/sbin', '/usr/sbin'] }
 
   ::secgen_functions::leak_files { 'lucee-flag-leak':
-    storage_directory => '/',
+    storage_directory => '/root',
     leaked_filenames  => $leaked_filenames,
     strings_to_leak   => $strings_to_leak,
     owner             => 'root',
