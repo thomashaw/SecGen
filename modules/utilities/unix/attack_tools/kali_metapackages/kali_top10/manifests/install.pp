8420243fbe79d8d4823be307ba0f245d452f7574
Z. Cliffe Schreuders <code.cliffe@schreuders.org>
2024-01-03 23:23:15 +0000

    kali repo updates

--- modules/utilities/unix/attack_tools/kali_metapackages/kali_top10/manifests/install.pp
+++ modules/utilities/unix/attack_tools/kali_metapackages/kali_top10/manifests/install.pp
@@ -6,6 +6,6 @@ class kali_top10::install{
 
   # The following is a workaround for the crackmapexec package triggering our IPS
   # almost everything in kali-tools-top10:
-  ensure_packages(['aircrack-ng', 'burpsuite', 'hydra', 'john', 'metasploit-framework', 'nmap', 'responder', 'sqlmap', 'wireshark'])
-  # except crackmapexec
+  ensure_packages(['burpsuite', 'john', 'metasploit-framework', 'nmap', 'responder', 'sqlmap', 'wireshark'])
+  # except crackmapexec, 'hydra', 'aircrack-ng',
 }
