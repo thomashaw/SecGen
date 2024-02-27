73808f3b530ec69b1c6c9ee95c6c642db60c85b4
Z. Cliffe Schreuders <code.cliffe@schreuders.org>
2024-02-27 16:35:56 +0000

    sudoedit updates

--- modules/vulnerabilities/unix/local/sudoedit/manifests/install.pp
+++ modules/vulnerabilities/unix/local/sudoedit/manifests/install.pp
@@ -4,27 +4,9 @@ class sudoedit::install {
   $secgen_parameters = secgen_functions::get_parameters($::base64_inputs_file)
   $leaked_filenames = $secgen_parameters['leaked_filenames']
   $strings_to_leak = $secgen_parameters['strings_to_leak']
-  $username = $secgen_parameters['unix_username'][0]
-  $password = $secgen_parameters['used_password'][0]
 
-  # Magic touch
-  # EDITOR='nano -- /etc/sudoers' sudoedit /etc/hosts
-
-  # This exploit relies on a user being in sudo group but we dont want access to everything! :) 
-  exec { 'goodbye-sudo':
-    command => "sed -i 's/%sudo/%root/' /etc/sudoers"
-  }
-  -> user { $username:
-    ensure   => present,
-    managehome => true,
-    # Make sure we are in the sudo group
-    groups => 'sudo',
-    shell    => '/bin/bash',
-    password => pw_hash($password, 'SHA-512', 'mysalt'),
-  }
-  # Let access to the hosts file via sudoedit
-  -> exec { 'i-can-edit-now':
-    command => "echo '${username} ALL=(ALL:ALL) sudoedit /etc/hosts' >> /etc/sudoers"
+  exec { 'i-can-sudoedit-now':
+    command => "echo 'ALL ALL = (root) NOPASSWD: sudoedit /etc/hosts' >> /etc/sudoers"
   }
   -> file { '/tmp/sudo_1.8.26-2_amd64.deb':
     ensure => file,
@@ -41,7 +23,7 @@ class sudoedit::install {
     storage_directory => '/root',
     leaked_filenames => $leaked_filenames,
     strings_to_leak => $strings_to_leak,
-    leaked_from => "",
+    leaked_from => "sudoedit",
     mode => '0600'
   }
 }
