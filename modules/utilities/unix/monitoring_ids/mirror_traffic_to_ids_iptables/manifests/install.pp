4ef7cc6890f5c9adfec380cecd0cbde8d23cfc76
thomashaw <thomashaw@gmail.com>
2024-10-16 14:01:14 +0100

    mirror_traffic_to_ids_iptables: rule update for IDS traffic mirroring

--- modules/utilities/unix/monitoring_ids/mirror_traffic_to_ids_iptables/manifests/install.pp
+++ modules/utilities/unix/monitoring_ids/mirror_traffic_to_ids_iptables/manifests/install.pp
@@ -5,9 +5,19 @@ class mirror_traffic_to_ids_iptables::install {
   ensure_packages(['iptables-persistent'])
 
   # force it to not be enabled because the interface in the config may be wrong
-  exec { 'save iptables':
+  exec { 'save iptables prerouting':
     path     => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
     command  => "iptables -t mangle -A PREROUTING -i `ls /sys/class/net | grep lo -v  | head -n1` -j TEE --gateway $ids_IP_address ; iptables-save > /etc/iptables/rules.v4",
+    provider => shell,
+  }->
+  exec { 'save iptables forward':
+    path     => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+    command  => "iptables -t mangle -A FORWARD -i `ls /sys/class/net | grep lo -v  | head -n1` -j TEE --gateway $ids_IP_address ; iptables-save > /etc/iptables/rules.v4",
+    provider => shell,
+  }->
+  exec { 'save iptables output':
+    path     => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+    command  => "iptables -t mangle -A OUTPUT -i `ls /sys/class/net | grep lo -v  | head -n1` -j TEE --gateway $ids_IP_address ; iptables-save > /etc/iptables/rules.v4",
     provider => shell,
   }->
   service { 'netfilter-persistent':
