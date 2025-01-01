962329e4ea2b5a196d806eb7754aa1771e3f5507
Z. Cliffe Schreuders <code.cliffe@schreuders.org>
2025-01-01 22:46:35 +0000

    Update pam_modules

--- modules/utilities/unix/authentication/pam_modules/manifests/install.pp
+++ modules/utilities/unix/authentication/pam_modules/manifests/install.pp
@@ -1,35 +1,35 @@
 class pam_modules::install {
   # Security and Access Control Modules
   # Provides brute force protection, two-factor auth, and biometric authentication
-  ensure_package(['libpam-abl', 'libpam-google-authenticator', 'libpam-oath'])
-  ensure_package(['libqrencode4'])
+  ensure_packages(['libpam-abl', 'libpam-google-authenticator', 'libpam-oath'])
+  ensure_packages(['libqrencode4'])
   # not installed: 'libpam-barada', 'libpam-biometric'
 
   # Authentication Backend Modules
   # Provides integration with various authentication systems like LDAP, MySQL, Kerberos
-  ensure_package(['libpam-ldapd', 'libpam-mysql', 'libpam-krb5',
+  ensure_packages(['libpam-ldapd', 'libpam-mysql', 'libpam-krb5',
                  'libpam-heimdal'])
 
   # Storage and Encryption Modules
   # Handles encrypted filesystems and mount operations
-  ensure_package(['libpam-mount', 'libpam-encfs', 'libpam-fscrypt'])
+  ensure_packages(['libpam-mount', 'libpam-encfs', 'libpam-fscrypt'])
 
   # Password Quality and Policy Modules
   # Enforces password strength and policies
-  ensure_package(['libpam-passwdqc', 'libpam-pwdfile', 'libpam-pwquality'])
+  ensure_packages(['libpam-passwdqc', 'libpam-pwdfile', 'libpam-pwquality'])
 
   # System Integration Modules
   # Handles system-level integration with cgroups, chroot, and namespaces
-  # ensure_package(['libpam-cgfs', 'libpam-cgroup', 'libpam-chroot',
+  # ensure_packages(['libpam-cgfs', 'libpam-cgroup', 'libpam-chroot',
   #               'libpam-net', 'libpam-cap'])
 
   # Smart Card and Hardware Token Modules
   # Enables authentication using PKCS#11 smart cards and hardware tokens
-  #ensure_package(['libpam-p11', 'libpam-pkcs11', 'libpam-poldi'])
+  #ensure_packages(['libpam-p11', 'libpam-pkcs11', 'libpam-poldi'])
 
   # Desktop Environment Integration
   # Provides integration with desktop environments and wallets
-  #ensure_package(['libpam-gnome-keyring', 'libpam-kwallet5',
+  #ensure_packages(['libpam-gnome-keyring', 'libpam-kwallet5',
   #               'libpam-kwallet-common'])
 
 }
