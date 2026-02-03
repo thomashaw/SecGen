9f635d44701b0f4aadf38827b90733524531b0bf
thomashaw <thomashaw@ts-MacBook-Air.home>
2026-02-03 14:08:37 +0000

    Copied vuln parameterised website + installing mysql and php

---
+++ modules/vulnerabilities/unix/http/vuln_parameterised_website/manifests/apache.pp
@@ -0,0 +1,23 @@
+class parameterised_website::apache {
+  $secgen_parameters = secgen_functions::get_parameters($::base64_inputs_file)
+  $port = $secgen_parameters['port'][0]
+
+  # essential packages for commando to function
+  package { ['php', 'php-mysqli', 'php-gd', 'libapache2-mod-php','mysql-server']:
+    ensure => installed,
+  }
+
+  class { '::apache':
+    default_vhost => false,
+    overwrite_ports => false,
+    mpm_module => 'prefork',
+  }
+
+  apache::vhost { 'parameterised.website':
+    port    => $port,
+    docroot => '/var/www/parameterised_website',
+    notify => Tidy['pws remove default site'],
+  }
+
+  ensure_resource('tidy','pws remove default site', {'path'=>'/etc/apache2/sites-enabled/000-default.conf'})
+}
