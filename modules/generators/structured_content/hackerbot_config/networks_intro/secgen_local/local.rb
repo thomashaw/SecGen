cf4f231a9fdb3515ee43d96cfeef9d50ec2f6979
thomashaw <thomashaw@gmail.com>
2026-03-26 16:33:04 +0000

    fix

--- modules/generators/structured_content/hackerbot_config/networks_intro/secgen_local/local.rb
+++ modules/generators/structured_content/hackerbot_config/networks_intro/secgen_local/local.rb
@@ -9,6 +9,7 @@ class NetworksIntro < HackerbotConfigGenerator
     self.module_name = 'Hackerbot Config Generator Networks Intro'
     self.title = 'Network Security Fundamentals: Network Design, Segmentation, NAT & DMZ'
 
+    self.ip_addresses = []
     self.local_dir = File.expand_path('../../',__FILE__)
     self.templates_path = "#{self.local_dir}/templates/"
     self.config_template_path = "#{self.local_dir}/templates/lab.xml.erb"
