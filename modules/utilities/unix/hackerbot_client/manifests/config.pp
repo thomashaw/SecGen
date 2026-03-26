e3d7f82867137b11d9c46f79590749e374d897c5
thomashaw <thomashaw@gmail.com>
2026-03-26 12:08:06 +0000

    More updates. Most of the config files are done. A few more non-standard ones to come.

--- modules/utilities/unix/hackerbot_client/manifests/config.pp
+++ modules/utilities/unix/hackerbot_client/manifests/config.pp
@@ -1,9 +1,8 @@
 class hackerbot_client::config {
   $secgen_parameters = secgen_functions::get_parameters($::base64_inputs_file)
-  $ssh_key_pair = parsejson($secgen_parameters['ssh_key_pair'][0])
-  $public_key   = $ssh_key_pair['public']
+  $ssh_key_pair  = parsejson($secgen_parameters['ssh_key_pair'][0])
+  $public_key    = $ssh_key_pair['public']
 
-  # /root/.ssh directory
   file { '/root/.ssh':
     ensure => directory,
     owner  => 'root',
@@ -11,26 +10,15 @@ class hackerbot_client::config {
     mode   => '0700',
   }
 
-  # Append Hackerbot's public key to authorized_keys
-  # ssh_authorized_key is idempotent — safe to apply repeatedly
-  ssh_authorized_key { 'hackerbot':
-    ensure  => present,
-    user    => 'root',
-    type    => 'ssh-rsa',
-    key     => split($public_key, ' ')[1],
-    require => File['/root/.ssh'],
-  }
-
-  # Ensure correct permissions on authorized_keys
   file { '/root/.ssh/authorized_keys':
     ensure  => file,
     owner   => 'root',
     group   => 'root',
     mode    => '0600',
-    require => Ssh_authorized_key['hackerbot'],
+    content => "${public_key}\n",
+    require => File['/root/.ssh'],
   }
 
-  # Allow root login via key but not password
   file_line { 'permit_root_login':
     path  => '/etc/ssh/sshd_config',
     line  => 'PermitRootLogin prohibit-password',
