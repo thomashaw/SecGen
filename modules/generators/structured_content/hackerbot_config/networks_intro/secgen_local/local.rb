9c49d51cde5e029733678fecc0adf25269d64c33
thomashaw <thomashaw@gmail.com>
2026-03-27 08:00:17 +0000

    wip - params not passing through properly? fix later.

--- modules/generators/structured_content/hackerbot_config/networks_intro/secgen_local/local.rb
+++ modules/generators/structured_content/hackerbot_config/networks_intro/secgen_local/local.rb
@@ -9,22 +9,23 @@ class NetworksIntro < HackerbotConfigGenerator
     self.module_name = 'Hackerbot Config Generator Networks Intro'
     self.title = 'Network Security Fundamentals: Network Design, Segmentation, NAT & DMZ'
 
-    self.ip_addresses = []
     self.local_dir = File.expand_path('../../',__FILE__)
     self.templates_path = "#{self.local_dir}/templates/"
     self.config_template_path = "#{self.local_dir}/templates/lab.xml.erb"
     self.html_template_path = "#{self.local_dir}/templates/labsheet.html.erb"
+
+    self.ip_addresses = []
   end
   def get_options_array
-    super + [['--IP_addresses', GetoptLong::REQUIRED_ARGUMENT]]
+    super + [['--ip_addresses', GetoptLong::REQUIRED_ARGUMENT]]
   end
 
   def process_options(opt, arg)
     super
     case opt
-    when '--IP_addresses'
-      self.ip_addresses = arg;
-    end
+      when '--ip_addresses'
+        self.ip_addresses << arg;
+      end
   end
 end
 
