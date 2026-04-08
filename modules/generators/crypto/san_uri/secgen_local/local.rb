801e28a3a6ceb80eb724b1106994efc19334c379
thomashaw <thomashaw@gmail.com>
2026-04-08 17:43:39 +0100

    progress

--- modules/generators/crypto/san_uri/secgen_local/local.rb
+++ modules/generators/crypto/san_uri/secgen_local/local.rb
@@ -4,27 +4,50 @@ require 'securerandom'
 
 class SanUriGenerator < StringEncoder
 
+  attr_accessor :domain, :protocol, :port
+
   def initialize
     super
     self.module_name = 'SAN URI Generator'
+    self.domain   = 'localhost'
+    self.protocol = 'http'
+    self.port     = ''
   end
 
   def encode_all
-    domain   = self.get_inputs['domain'].first || 'localhost'
-    protocol = self.get_inputs['protocol'].first || 'http'
-    port     = self.get_inputs['port'].first
-
-    # Generate a random hex path — 16 bytes = 32 hex chars, unguessable
     hex_path = SecureRandom.hex(16)
 
-    # Build URI — omit port if not supplied or empty
-    if port && !port.empty?
-      uri = "#{protocol}://#{domain}:#{port}/#{hex_path}/"
+    if self.port && !self.port.empty?
+      uri = "#{self.protocol}://#{self.domain}:#{self.port}/#{hex_path}/"
     else
-      uri = "#{protocol}://#{domain}/#{hex_path}/"
+      uri = "#{self.protocol}://#{self.domain}/#{hex_path}/"
     end
 
     self.outputs << uri
+  end
+
+  def process_options(opt, arg)
+    super
+    case opt
+    when '--domain'
+      self.domain = arg
+    when '--protocol'
+      self.protocol = arg
+    when '--port'
+      self.port = arg
+    end
+  end
+
+  def get_options_array
+    super + [
+      ['--domain',   GetoptLong::REQUIRED_ARGUMENT],
+      ['--protocol', GetoptLong::REQUIRED_ARGUMENT],
+      ['--port',     GetoptLong::REQUIRED_ARGUMENT],
+    ]
+  end
+
+  def encoding_print_string
+    "domain: #{self.domain}, protocol: #{self.protocol}, port: #{self.port}"
   end
 
 end
--- modules/generators/crypto/self_signed_cert/secgen_local/local.rb
+++ modules/generators/crypto/self_signed_cert/secgen_local/local.rb
@@ -1,25 +1,82 @@
 #!/usr/bin/ruby
 require_relative '../../../../../lib/objects/local_string_encoder.rb'
-require 'open3'
+require 'openssl'
 
-class SSHKeyPairGenerator < StringEncoder
+class SelfSignedCertGenerator < StringEncoder
+
+  attr_accessor :domain, :strings_to_leak, :san_uri, :key_size, :days
 
   def initialize
     super
-    self.module_name = 'GPG Key Pair Generator'
+    self.module_name     = 'Self-Signed TLS Certificate Generator'
+    self.domain          = 'localhost'
+    self.strings_to_leak = []
+    self.san_uri         = ''
+    self.key_size        = 2048
+    self.days            = 365
   end
 
   def encode_all
-    # TODO: Incorporate some way of making this key import temporary... We don't want to fill our host systems keyring up with a million keys.
-    _, gen_stderr, _ = Open3.capture3("gpg --batch --gen-key #{GENERATORS_DIR}key/gpg_key_pair/files/parameterfile")
-    key_id = gen_stderr.split("\n").last.split(" ")[2]
+    cn = self.strings_to_leak[0] || self.domain
 
-    privkey_stdout, _, _ = Open3.capture3("gpg --armor --export-secret-keys #{key_id}")
-    pubkey_stdout, _, _ = Open3.capture3("gpg --armor --export #{key_id}")
+    key = OpenSSL::PKey::RSA.new(self.key_size)
 
-    self.outputs << {'private' => privkey_stdout, 'public' => pubkey_stdout}.to_json
+    cert = OpenSSL::X509::Certificate.new
+    cert.version    = 2
+    cert.serial     = rand(1_000_000_000)
+    cert.not_before = Time.now
+    cert.not_after  = Time.now + (self.days * 24 * 60 * 60)
+
+    subject = OpenSSL::X509::Name.parse("/CN=#{cn}/O=SecGen Lab/OU=Netsec")
+    cert.subject    = subject
+    cert.issuer     = subject
+    cert.public_key = key.public_key
+
+    ef = OpenSSL::X509::ExtensionFactory.new
+    ef.subject_certificate = cert
+    ef.issuer_certificate  = cert
+
+    san_entries = ["DNS:#{self.domain}"]
+    san_entries << "URI:#{self.san_uri}" if self.san_uri && !self.san_uri.empty?
+
+    cert.add_extension(ef.create_extension('subjectAltName', san_entries.join(','), false))
+    cert.add_extension(ef.create_extension('basicConstraints', 'CA:FALSE', false))
+
+    cert.sign(key, OpenSSL::Digest::SHA256.new)
+
+    self.outputs << {'cert_pem' => cert.to_pem, 'key_pem' => key.to_pem, 'cn' => cn}.to_json
+  end
+
+  def process_options(opt, arg)
+    super
+    case opt
+    when '--domain'
+      self.domain = arg
+    when '--strings_to_leak'
+      self.strings_to_leak << arg
+    when '--san_uri'
+      self.san_uri = arg
+    when '--key_size'
+      self.key_size = arg.to_i
+    when '--days'
+      self.days = arg.to_i
+    end
+  end
+
+  def get_options_array
+    super + [
+      ['--domain',          GetoptLong::REQUIRED_ARGUMENT],
+      ['--strings_to_leak', GetoptLong::REQUIRED_ARGUMENT],
+      ['--san_uri',         GetoptLong::REQUIRED_ARGUMENT],
+      ['--key_size',        GetoptLong::REQUIRED_ARGUMENT],
+      ['--days',            GetoptLong::REQUIRED_ARGUMENT],
+    ]
+  end
+
+  def encoding_print_string
+    "domain: #{self.domain}, cn: #{self.strings_to_leak[0]}, san_uri: #{self.san_uri}"
   end
 
 end
 
-SSHKeyPairGenerator.new.run
+SelfSignedCertGenerator.new.run
