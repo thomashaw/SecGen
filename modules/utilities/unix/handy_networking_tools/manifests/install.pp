04c1b6b35221a4110ee5c166dfd2327fc0b1a66e
thomashaw <thomashaw@ts-MacBook-Air.home>
2026-04-13 14:26:44 +0100

    cutting down noise with ntp spam

--- modules/utilities/unix/handy_networking_tools/manifests/install.pp
+++ modules/utilities/unix/handy_networking_tools/manifests/install.pp
@@ -1,4 +1,15 @@
 class handy_networking_tools::install{
+
+  $secgen_parameters = secgen_functions::get_parameters($::base64_inputs_file)
+  $disable_ntp = downcase($secgen_parameters['disable_ntp'][0])
+
+  if $disable_ntp == 'true'{
+    service { 'systemd-timesyncd':
+      ensure => stopped,
+      enable => false,
+    }
+  }
+
   ensure_packages([
     'resolvconf',
     'dnsutils',
