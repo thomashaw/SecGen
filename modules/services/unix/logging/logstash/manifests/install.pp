f70ce739a503217e3ab7f0b0d6001dfc42c5a23f
thomashaw <thomashaw@ts-MacBook-Air.home>
2026-04-24 14:11:57 +0100

    EXPERIMENTAL: Trying to go directly from logstash -> alert_router.rb

--- modules/services/unix/logging/logstash/manifests/install.pp
+++ modules/services/unix/logging/logstash/manifests/install.pp
@@ -8,4 +8,10 @@ class logstash::install () {
     source => 'puppet:///modules/logstash/combined_path.rb',
     require => Package['logstash'],
   }
+
+  exec { 'install-logstash-output-exec':
+    command => '/usr/share/logstash/bin/logstash-plugin install logstash-output-exec',
+    require => Package['logstash'],
+    timeout => 600,
+  }
 }
